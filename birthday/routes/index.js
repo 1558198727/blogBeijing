ed96ebfc53307df439486bceb991559831e61ef3
liyunhao <1558198727@qq.com>
2018-01-27 22:51:02 +0800

    127

--- birthday/routes/index.js
+++ birthday/routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    var date = new Date();
+
+    var file = "C:\\BirthdayLog.txt";
+    var write_date = "当前访问的时间为" + date + '\r\n';
+    console.log(write_date);
+    fs.appendFile(file, write_date, function(err){
+        if(err)
+            console.log("fail " + err);
+        else
+            console.log("写入文件ok");
+    });
+
   res.render('index', { title: 'index' });
 });
 
@@ -19,5 +31,6 @@ router.get('/Memories', function(req, res, next) {
     console.log("Memories");
     res.render('Memories', { title: 'Memories' });
 });
+
 
 module.exports = router;

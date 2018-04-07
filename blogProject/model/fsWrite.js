0ec7a3401805afeb51561669782fb590b2f1ef52
1558198727@qq.com <1558198727@qq.com>
2018-04-07 13:44:56 +0800

    date

--- blogProject/model/fsWrite.js
+++ blogProject/model/fsWrite.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
 
 // 文件读写
-
 exports.WriteBlogLog = function (page) {
     var date = new Date();
-    var file = "C:\\BlogLog.txt";
+    var file = "C:\\blogLog\\"+getNowFormatDate()+".txt";
+    console.log("file: "+ file);
     var write_date = "当前访问的时间为" + date + '\r\n'+page +'\r\n';
-    // console.log(write_date);
+    console.log(write_date);
     fs.appendFile(file, write_date, function(err){
         if(err)
             console.log("fail " + err);
@@ -39,3 +39,19 @@ exports.WriteUsersInfo = function (info) {
             console.log(write_date);
     });
 };
+//获取当前时间，格式YYYY-MM-DD
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = year + seperator1 + month + seperator1 + strDate;
+    return currentdate;
+}
